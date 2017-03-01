db8a196863e3b5ba203dcd6e06afaf13bcdbb673
Ignasi Marimon-Clos <ignasi@lightbend.com>
2017-03-01 17:45:58 +0100

    Some hack code to smoe test #564

--- helloscala-impl/src/main/scala/com/example/helloscala/impl/HelloscalaLoader.scala
+++ helloscala-impl/src/main/scala/com/example/helloscala/impl/HelloscalaLoader.scala
@@ -1,20 +1,45 @@
 package com.example.helloscala.impl
 
+import com.example.helloscala.api.HelloscalaService
+import com.lightbend.lagom.scaladsl.api.{AdditionalConfiguration, ProvidesAdditionalConfiguration}
+import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
 import com.lightbend.lagom.scaladsl.persistence.cassandra.CassandraPersistenceComponents
 import com.lightbend.lagom.scaladsl.server._
-import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
-import play.api.libs.ws.ahc.AhcWSComponents
-import com.example.helloscala.api.HelloscalaService
 import com.softwaremill.macwire._
 import com.typesafe.conductr.bundlelib.lagom.scaladsl.ConductRApplicationComponents
+import com.typesafe.config.ConfigFactory
+import play.api.Configuration
+import play.api.libs.ws.ahc.AhcWSComponents
+
+import scala.util.Try
 
 class HelloscalaLoader extends LagomApplicationLoader {
 
   override def load(context: LagomApplicationContext): LagomApplication =
-    new HelloscalaApplication(context) with ConductRApplicationComponents
+    new HelloscalaApplication(context) with ConductRApplicationComponents {
+      conductRConfiguration.entrySet.map(_.toString).filter(in => in.contains("seed") || in.contains("join-self")).foreach(x => println(s"fooooConductr : $x"))
+      configuration.entrySet.map(_.toString).filter(in => in.contains("seed") || in.contains("join-self")).foreach(x => println(s"fooooFINAL : $x"))
+    }
+
+  trait foo extends ProvidesAdditionalConfiguration {
+    lazy val x: Configuration = Configuration(ConfigFactory.parseString(
+      """
+        |akka.cluster.seed-nodes.0="akka.tcp://helloscala-impl-1@192.168.10.1:10410"
+        |foo.seed=bar
+      """.stripMargin))
+    override def additionalConfiguration: AdditionalConfiguration = super.additionalConfiguration ++ x
+  }
 
   override def loadDevMode(context: LagomApplicationContext): LagomApplication =
-    new HelloscalaApplication(context) with LagomDevModeComponents
+    new HelloscalaApplication(context) with LagomDevModeComponents with foo {
+      (1 to 3).foreach(_ => println("-" * 40))
+
+      private val listx = x.getList("akka.cluster.seed-nodes").get.unwrapped()
+      println("Is akka.cluster.seed-nodes provided? " + listx.get(0))
+      private val list = configuration.getList("akka.cluster.seed-nodes").get.unwrapped()
+      println("Is akka.cluster.seed-nodes maintained? " + Try(list.get(0)))
+      (1 to 3).foreach(_ => println("*" * 40))
+    }
 
   override def describeServices = List(
     readDescriptor[HelloscalaService]
